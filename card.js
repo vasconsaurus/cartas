0d423fdf5ed334f44723a38d71ed72178a1e55d5
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-08-23 16:43:53 -0300

    added different cards
    I added different cards to cardContent, and am displaying them
    by using a map. So we avoid bugs in the future, I added a key to
    each. Since the names wont repeat, I'm using them as keys.

--- card.js
+++ card.js
@@ -1,7 +1,10 @@
 const root = document.getElementById('container')
 
-const card_content = {
-  lua: "A princípio, os dois cães, as duas torres e as gotas atraídas pela lua são símbolos fortíssimos que anunciam a necessidade de reflexão das direções."
+const cardContent = {
+  lua: "A princípio, os dois cães, as duas torres e as gotas atraídas pela lua são símbolos fortíssimos que anunciam a necessidade de reflexão das direções.",
+  sol: "Não poderia ser diferente a mensagem quando o assunto é o O Sol. Sua luz é sinal de positividade e está completamente associado aos objetivos e sucesso.",
+  mundo: "É aqui que aparece as conquistas feitas, os aprendizados adquiridos, as barreiras rompidas e toda a batalha travada.",
+  estrela: "Não é de se admirar que a estrela represente em nossas vidas sinal de luz e renovação. Sem essa âncoras é possível agora caminhar em direção a uma nova vida."
 }
 
 function Card({cardName, cardText}) {
@@ -19,8 +22,9 @@ function handleClick() {
 
 const element = (
   <div className="cards_container">
-    <Card cardName={Object.keys(card_content)} cardText={Object.values(card_content)}></Card>
-    <Card cardName={Object.keys(card_content)} cardText={Object.values(card_content)}></Card>
+    {Object.entries(cardContent).map(itemArr => (
+    <Card cardName={itemArr[0]} cardText={Object.values(itemArr[1])} key={itemArr[0]}></Card>
+    ))}
   </div>
   )
 

cab863810e34132ffbce367811fbb628a28a698d
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-08-23 18:40:09 -0300

    Added a card

--- card.js
+++ card.js
@@ -4,7 +4,7 @@ const cardContent = {
   lua: "A princípio, os dois cães, as duas torres e as gotas atraídas pela lua são símbolos fortíssimos que anunciam a necessidade de reflexão das direções.",
   sol: "Não poderia ser diferente a mensagem quando o assunto é o O Sol. Sua luz é sinal de positividade e está completamente associado aos objetivos e sucesso.",
   mundo: "É aqui que aparece as conquistas feitas, os aprendizados adquiridos, as barreiras rompidas e toda a batalha travada.",
-  estrela: "Não é de se admirar que a estrela represente em nossas vidas sinal de luz e renovação. Sem essa âncoras é possível agora caminhar em direção a uma nova vida."
+  estrela: "Não é de se admirar que a estrela represente em nossas vidas sinal de luz e renovação. Sem essa âncora é possível agora caminhar em direção a uma nova vida."
 }
 
 function Card({cardName, cardText}) {

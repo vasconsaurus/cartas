b7056938130889a0ef3f7a26c02ed981f9010950
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-08-23 14:53:37 -0300

    Created a card component
    Still very simple, going to move foward and see if there is a better
    way to do it.

--- card.js
+++ card.js
@@ -4,15 +4,19 @@ const card_content = {
   lua: "A princípio, os dois cães, as duas torres e as gotas atraídas pela lua são símbolos fortíssimos que anunciam a necessidade de reflexão das direções."
 }
 
-function message({children}) {
-  return <div className="message">{children}</div>
+function Card({children}) {
+  return (
+    <div className="card">
+      {children}
+    </div>
+  );
 }
 
 const element = (
-  <div className="card">
+  <Card className="card">
     <h3>{Object.keys(card_content)}</h3>
     <p>{Object.values(card_content)}</p>
-  </div>
+  </Card>
 )
 
 ReactDOM.createRoot(root).render(element)

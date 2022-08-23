7031bfdf2312c2e9733b5612e5c79a4b6cf19507
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-08-23 14:24:32 -0300

    Created the first card
    A very simple first card. Next I want to create a proper
    component, add about 3 cards, and iterate through those.

--- card.js
+++ card.js
@@ -1,12 +1,18 @@
-    function message({children}) {
-      return <div className="message">{children}</div>
-    }
+const root = document.getElementById('cards_container')
 
-    const element = (
-      <div className="container">
-        {message({children: 'Hello World'})}
-        {message({children: 'Goodbye World'})}
-      </div>
-    )
+const card_content = {
+  lua: "A princípio, os dois cães, as duas torres e as gotas atraídas pela lua são símbolos fortíssimos que anunciam a necessidade de reflexão das direções."
+}
 
-ReactDOM.createRoot(document.getElementById('cards_container')).render(element)
+function message({children}) {
+  return <div className="message">{children}</div>
+}
+
+const element = (
+  <div className="card">
+    <h3>{Object.keys(card_content)}</h3>
+    <p>{Object.values(card_content)}</p>
+  </div>
+)
+
+ReactDOM.createRoot(root).render(element)

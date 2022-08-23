f55c6809109b58f5127a3612ccc4fa43fe20e242
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-08-23 14:59:45 -0300

    Edited how I wrote the component a bit
    Right now I'm just testing what might work better. I think this
    could be more flexible, less repetitive.

--- card.js
+++ card.js
@@ -4,19 +4,17 @@ const card_content = {
   lua: "A princípio, os dois cães, as duas torres e as gotas atraídas pela lua são símbolos fortíssimos que anunciam a necessidade de reflexão das direções."
 }
 
-function Card({children}) {
+function Card({cardName, cardText}) {
   return (
     <div className="card">
-      {children}
+      <h3>{cardName}</h3>
+      <p>{cardText}</p>
     </div>
   );
 }
 
 const element = (
-  <Card className="card">
-    <h3>{Object.keys(card_content)}</h3>
-    <p>{Object.values(card_content)}</p>
-  </Card>
+  <Card className="card" cardName={Object.keys(card_content)} cardText={Object.values(card_content)} />
 )
 
 ReactDOM.createRoot(root).render(element)
